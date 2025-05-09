b058f21ff88d857f67c8f1ceb7de91a58a5a2c34
sawaidbasit <--unset>
2025-05-10 02:48:46 +0500

    fix responsiveness in Table List

--- src/components/StationList.tsx
+++ src/components/StationList.tsx
@@ -102,8 +102,7 @@ const StationListingPage = () => {
   }, [stateName]);
   
   return (
-    <div className="min-h-screen bg-zinc-50 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-
+    <div className="min-h-screen max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
       {loading ? (
         <p className="text-center text-gray-600">Loading...</p>
       ) : Object.keys(groupedData).length === 0 ? (
@@ -111,38 +110,89 @@ const StationListingPage = () => {
       ) : (
         Object.entries(groupedData).map(([location, stations]) => (
           <div key={location} className="mb-12">
-            <h1 className="text-3xl font-bold text-green-600 mb-4">{location}</h1>
+            <h1 className="text-3xl font-bold text-green-600 mb-4">
+              {location}
+            </h1>
 
-            <div className="overflow-x-auto mt-6 bg-white rounded-xl shadow-xl border border-gray-200">
-              <table className="min-w-full divide-y divide-gray-200">
-                <thead className="bg-gray-100">
-                  <tr>
-                    <th className="px-6 py-4 text-left text-lg font-bold text-gray-700">Station Name</th>
-                    <th className="px-6 py-4 text-left text-lg font-bold text-gray-700">Petrol</th>
-                    <th className="px-6 py-4 text-left text-lg font-bold text-gray-700">Diesel</th>
-                    <th className="px-6 py-4 text-left text-lg font-bold text-gray-700">Kerosene</th>
-                  </tr>
-                </thead>
-                <tbody className="divide-y divide-gray-300">
-                  {stations.map((station, index) => (
-                    <tr key={index} className="hover:bg-zinc-50 transition">
-                      <td className="px-6 py-4 text-gray-900 font-medium flex items-center gap-2">
-                        <Fuel className="w-8 h-6 text-blue-500" />
-                        {station.station_name}
-                      </td>
-                      <td className="px-6 py-4 font-semibold text-green-600">
-                        {station.petrol ? `₦${station.petrol}/L` : 'N/A'}
-                      </td>
-                      <td className="px-6 py-4 text-green-600 font-semibold">
-                        {station.diesel ? `₦${station.diesel}/L` : 'N/A'}
-                      </td>
-                      <td className="px-6 py-4 text-green-600 font-semibold">
-                        {station.kerosene ? `₦${station.kerosene}/L` : 'N/A'}
-                      </td>
+            <div className="max-sm:hidden ">
+              <div className="overflow-x-auto mt-6 bg-white rounded-xl shadow-xl border border-gray-200">
+                <table className="min-w-full divide-y divide-gray-200">
+                  <thead className="bg-gray-100">
+                    <tr>
+                      <th className="px-6 py-4 text-left text-lg font-bold text-gray-700">
+                        Station Name
+                      </th>
+                      <th className="px-6 py-4 text-left text-lg font-bold text-gray-700">
+                        Petrol
+                      </th>
+                      <th className="px-6 py-4 text-left text-lg font-bold text-gray-700">
+                        Diesel
+                      </th>
+                      <th className="px-6 py-4 text-left text-lg font-bold text-gray-700">
+                        Kerosene
+                      </th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
+                  </thead>
+                  <tbody className="divide-y divide-gray-300">
+                    {stations.map((station, index) => (
+                      <tr key={index} className="hover:bg-zinc-50 transition">
+                        <td className="px-6 py-4 text-gray-900 font-medium flex items-center gap-2">
+                          <Fuel className="w-8 h-6 text-blue-500" />
+                          {station.station_name}
+                        </td>
+                        <td className="px-6 py-4 font-semibold text-green-600">
+                          {station.petrol ? `₦${station.petrol}/L` : "N/A"}
+                        </td>
+                        <td className="px-6 py-4 text-green-600 font-semibold">
+                          {station.diesel ? `₦${station.diesel}/L` : "N/A"}
+                        </td>
+                        <td className="px-6 py-4 text-green-600 font-semibold">
+                          {station.kerosene ? `₦${station.kerosene}/L` : "N/A"}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+
+            <div className="max-sm:block hidden">
+              {stations.map((station, index) => (
+                <div
+                  key={index}
+                  className="mt-6 p-6 bg-white rounded-xl shadow-xl border border-gray-200"
+                >
+                  <h3 className="text-2xl text-gray-900 font-bold flex items-center gap-1">
+                    <Fuel className="w-10 h-8 text-blue-500" />
+                    {station.station_name}
+                  </h3>
+
+                  <div className="mt-5">
+                    <div className="flex justify-between items-center bg-gray-200 rounded-lg px-4 py-2 mt-3">
+                      <span className="text-gray-600 font-medium">Petrol</span>
+                      <span className="text-lg font-bold text-green-600">
+                        {station.petrol ? `₦${station.petrol}/L` : "NA"}
+                      </span>
+                    </div>
+
+                    <div className="flex justify-between items-center bg-gray-200 rounded-lg px-4 py-2 mt-3">
+                      <span className="text-gray-600 font-medium">Diesel</span>
+                      <span className="text-lg font-bold text-green-600">
+                        {station.diesel ? `₦${station.diesel}/L` : "NA"}
+                      </span>
+                    </div>
+
+                    <div className="flex justify-between items-center bg-gray-200 rounded-lg px-4 py-2 mt-3">
+                      <span className="text-gray-600 font-medium">
+                        Kerosene
+                      </span>
+                      <span className="text-lg font-bold text-green-600">
+                        {station.kerosene ? `₦${station.kerosene}/L` : "NA"}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
         ))
